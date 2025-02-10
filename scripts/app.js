25dae59e9cd2b075b079192006b5065c9aee1f02
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-10 15:24:40 +0800

    Use setInterval() for countdown timer functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
 // 程式碼寫這裡
+const timer = document.querySelector(".timer");
+let totalSeconds = 120;
+
+// 設定時鐘呈現的內容
+function updateTimer(seconds) {
+  let mins = Math.floor(seconds / 60);
+  let secs = seconds % 60;
+
+  timer.textContent = `${mins}:${secs}`;
+}
+setInterval(() => {
+  totalSeconds--;
+  updateTimer(totalSeconds);
+  //console.log(totalSeconds);
+}, 1000); // 1000代表是間隔時間 1000ms
