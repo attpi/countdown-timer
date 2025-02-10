dd911240505c05274cfe49e180fd3160a77a769a
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-10 15:38:45 +0800

    Fix countdown restart issue after stopping (totalSeconds decreasing)

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 // 程式碼寫這裡
 const timer = document.querySelector(".timer");
-let totalSeconds = 120;
+let defaultSesonds = 5;
+let totalSeconds = 0;
 let running = false;
 
 // 設定時鐘呈現的內容
@@ -10,12 +11,26 @@ function updateTimer(seconds) {
 
   timer.textContent = `${mins}:${secs}`;
 }
+
+function timesup() {
+  clearInterval(timeID);
+  running = false;
+  //updateTimer(0);
+}
+
 function initTimer() {
   running = true;
-  setInterval(() => {
-    totalSeconds--;
-    updateTimer(totalSeconds);
-    //console.log(totalSeconds);
+  totalSeconds = defaultSesonds;
+  // 設定時間替換的方式
+  timeID = setInterval(() => {
+    if (totalSeconds > 0) {
+      totalSeconds--;
+      updateTimer(totalSeconds);
+      //console.log(totalSeconds);
+    } else {
+      console.log("stop");
+      timesup();
+    }
   }, 1000); // 1000代表是間隔時間 1000ms
 }
 
